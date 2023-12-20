7008d8a1d0b4182440be1671a08c777a4f31cb2b
Niroj Ghale <ghalenir@gmail.com>
2023-12-20 14:10:24 -0500

    Some code cleanup

--- app/services/index.ts
+++ app/services/index.ts
@@ -6,15 +6,17 @@ const BASE_API = process.env.NEXT_PUBLIC_BASE_API;
 export const getBookingList = async () => {
 
   // const current_local_date = moment('20231214').tz("America/New_York").format("YYYY-MM-DD");
-  const current_local_date = moment().tz("America/New_York").format("YYYY-MM-DD");
+  // const current_local_date = moment().tz("America/New_York").format("YYYY-MM-DD");
   
+  const start_date = moment().add(-15, 'day').format("YYYY-MM-DD");
+  const end_date = moment().add(15, 'day').format("YYYY-MM-DD");
 
-  console.log(moment('2023-12-14T00:00').tz("America/New_York"),"Niroj Gahle ------------------------- ");
-  console.log(moment(`${current_local_date}T00:00`).tz("UTC").format("YYYYMMDD"),"Starting");
-  console.log(moment(`${current_local_date}T24:00`).tz("UTC").format("YYYYMMDD"),"Ending");
+  // console.log(moment('2023-12-14T00:00').tz("America/New_York"),"Niroj Gahle ------------------------- ");
+  // console.log(moment(`${current_local_date}T00:00`).tz("UTC").format("YYYYMMDD"),"Starting");
+  // console.log(moment(`${current_local_date}T24:00`).tz("UTC").format("YYYYMMDD"),"Ending");
 
-  console.log(moment('2023-12-14T00:00').tz("UTC"),"Starting");
-  console.log(moment('2023-12-14T24:00').tz("UTC"),"Ending");
+  // console.log(moment('2023-12-14T00:00').tz("UTC"),"Starting");
+  // console.log(moment('2023-12-14T24:00').tz("UTC"),"Ending");
 
   try {
     // const min_date = moment().format("YYYYMMDD");
@@ -25,8 +27,8 @@ export const getBookingList = async () => {
     // https://irislashinc.com/api/booking?field_date_range_value[min]=2023-12-13-05-00&field_date_range_value[max]=2023-12-14-0500
 
 
-    const min_date = moment(`${current_local_date}T00:00`).tz("UTC").format("YYYYMMDDHHSS")
-    const max_date = moment(`${current_local_date}T24:00`).tz("UTC").format("YYYYMMDDHHSS")
+    const min_date = moment(`${start_date}T00:00`).tz("UTC").format("YYYYMMDDHHSS")
+    const max_date = moment(`${end_date}T24:00`).tz("UTC").format("YYYYMMDDHHSS")
 
     const booking_api_url = `${BASE_API}/booking?field_date_range_value[min]=${min_date}&field_date_range_value[max]=${max_date}`;
     console.log(booking_api_url);
